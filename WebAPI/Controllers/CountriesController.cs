a00ab0e47ad065c581882d80b90a77a25fdc48dc
cg-dev <git@myonlineaddress.co.uk>
2015-02-26 00:38:40 +0000

    Add WebAPI with countries controller and attempt to retrieve data for display. Not working fully yet!

---
+++ WebAPI/Controllers/CountriesController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace Angular.Controllers
+{
+    using CodePatterns.Model;
+
+    public class CountriesController : ApiController
+    {
+        private List<Country> _countries;
+
+        public CountriesController()
+        {
+            _countries = new List<Country>{
+                    new Country {Id = 1, Name = "England"},
+                    new Country {Id = 2, Name = "Wales"},
+                    new Country {Id = 3, Name = "Scotland"},
+                    new Country {Id = 4, Name = "Ireland"},
+                    new Country {Id = 5, Name = "France"},
+                    new Country {Id = 6, Name = "Italy"}
+            };
+        }
+
+        // GET api/<controller>
+        public IEnumerable<Country> Get()
+        {
+            return _countries;
+        }
+
+        // GET api/<controller>/5
+        public Country Get(int id)
+        {
+            var country = _countries.SingleOrDefault(c => c.Id == id);
+
+            if (country != null)
+            {
+                return country;
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
