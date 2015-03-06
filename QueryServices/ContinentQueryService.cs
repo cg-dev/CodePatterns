66e1ddfdc392fc2862301a13da0fcdc198b915bf
Chris Garnett <chris.garnett@open.ac.uk>
2015-03-06 13:51:41 +0000

    Tables on Angular.Index.html populated from WebAPI > QueryService > Entity Framework.

--- QueryServices/ContinentQueryService.cs
+++ QueryServices/ContinentQueryService.cs
@@ -5,25 +5,21 @@ namespace QueryServices
 {
     public class ContinentQueryService
     {
-        private CodePatternsConnectionString _context;
-
-        public ContinentQueryService()
-        {
-            _context = new CodePatternsConnectionString();
-        }
-
-        // GET api/<controller>
         public IEnumerable<Continent> Get()
         {
-            var continents = new List<Continent>();
 
-            foreach (var continent in _context.Continents)
+            var continents = new List<Continent>(); 
+            
+            using (var context = new CodePatternsContext())
             {
-                continents.Add(new Continent
-                               {
-                                   Id = continent.Id,
-                                   Name = continent.Name
-                               });
+                foreach (var continent in context.Continents)
+                {
+                    continents.Add(new Continent
+                    {
+                        Id = continent.Id,
+                        Name = continent.Name
+                    });
+                }
             }
 
             return continents;
