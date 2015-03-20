680b54968ed8a624963bfdde44ea2b7771a1dd53
Chris Garnett <chris.garnett@open.ac.uk>
2015-03-20 14:55:13 +0000

    Move simple directive into a template file.

--- Angular/Controllers/app.js
+++ Angular/Controllers/app.js
@@ -1,9 +1,17 @@
 ﻿var app = angular.module("angularApp", ['ngRoute', 'ngResource', 'ui.bootstrap']);
 
-app.directive('simpleDirective', function() {
+//app.directive('simpleDirective', function() {
+//    return {
+//        restrict: 'AE',
+//        replace: 'true',
+//        template: '<h4>A bit of text that has been constructed by a Simple Directive</h4>'
+//    };
+//});
+
+app.directive('simpleDirective', function () {
     return {
         restrict: 'AE',
         replace: 'true',
-        template: '<h4>A bit of text that has been constructed by a Simple Directive</h4>'
+        templateUrl: '/Templates/simpleDirective.html'
     };
 });
