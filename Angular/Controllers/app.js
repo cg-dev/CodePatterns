abab36918652dd59880ae67a7b048383c05d8241
Chris Garnett <chris.garnett@open.ac.uk>
2015-03-20 14:23:31 +0000

    Add a simple directive

--- Angular/Controllers/app.js
+++ Angular/Controllers/app.js
@@ -1,1 +1,9 @@
 ﻿var app = angular.module("angularApp", ['ngRoute', 'ngResource', 'ui.bootstrap']);
+
+app.directive('simpleDirective', function() {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        template: '<h4>A bit of text that has been constructed by a Simple Directive</h4>'
+    };
+});
