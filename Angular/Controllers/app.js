28605b6eb8d2068f26a508665249f3bf8f2b9c85
Chris Garnett <chris.garnett@open.ac.uk>
2015-03-23 17:32:29 +0000

    Attempting to pass parameters into a directive.

--- Angular/Controllers/app.js
+++ Angular/Controllers/app.js
@@ -1,17 +1,20 @@
 ﻿var app = angular.module("angularApp", ['ngRoute', 'ngResource', 'ui.bootstrap']);
 
-//app.directive('simpleDirective', function() {
-//    return {
-//        restrict: 'AE',
-//        replace: 'true',
-//        template: '<h4>A bit of text that has been constructed by a Simple Directive</h4>'
-//    };
-//});
-
 app.directive('simpleDirective', function () {
     return {
         restrict: 'AE',
+        scope: {
+            sdValue: '@'
+        },
         replace: 'true',
-        templateUrl: '/Templates/simpleDirective.html'
+        templateUrl: '/Templates/simpleDirective.html',
+        link: function($scope, element, attrs) {
+            element.bind('mouseenter', function() {
+                element.css('background-color', 'yellow');
+            });
+            element.bind('mouseleave', function () {
+                element.css('background-color', 'white');
+            });
+        }
     };
 });
