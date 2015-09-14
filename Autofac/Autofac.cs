01d7b6271529a22e04db7d2a5cfe2321edf11301
cg-dev <chris@myonlineaddress.co.uk>
2015-09-14 23:33:04 +0100

    Added console application to use web service declared as a web service. Not using Autofac.

--- Autofac/Autofac.cs
+++ Autofac/Autofac.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Autofac
 {
-    public class Autofac
+    class Program
     {
+        static void Main()
+        {
+            var service = new w3schools.TempConvert(); // Added as web reference
+
+            //var builder = new ContainerBuilder();
+            //builder.RegisterType<com.w3schools.www.TempConvert>().As<IService>();
+            //var container = builder.Build();
+
+            //var service = container.Resolve<IService>();
+
+            Console.WriteLine("Celsius: {0} = farenheit, {1}", 0, service.CelsiusToFahrenheit("0"));
+            Console.WriteLine("Celsius: {0} = farenheit, {1}", 16, service.CelsiusToFahrenheit("16"));
+            Console.WriteLine("Celsius: {0} = farenheit, {1}", 100, service.CelsiusToFahrenheit("100"));
+            Console.WriteLine("Farenheit: {0} = celsius, {1}", 0, service.FahrenheitToCelsius("0"));
+            Console.WriteLine("Farenheit: {0} = celsius, {1}", 32, service.FahrenheitToCelsius("32"));
+            Console.WriteLine("Farenheit: {0} = celsius, {1}", 100, service.FahrenheitToCelsius("100"));
+            Console.ReadLine();
+        }
     }
 }
---
+++ Autofac/IService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autofac
+{
+    interface IService
+    {
+        string CelsiusToFahrenheit(string celsius);
+        string FahrenheitToCelsius(string farenheit);
+    }
+}
