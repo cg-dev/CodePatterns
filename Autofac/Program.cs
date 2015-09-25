0089d122e30a0a12e1c919afbffbfe806ff4e55d
cg-dev <chris@myonlineaddress.co.uk>
2015-09-25 23:25:33 +0100

    Files missing from original Autofac push

---
+++ Autofac/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Autofac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MessageA>().As<IMessage>();
+            builder.RegisterType<Logic>();
+
+            var container = builder.Build();
+
+            var logic = container.Resolve<Logic>();
+
+            logic.Run();
+        }
+    }
+}
