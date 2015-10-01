48b108feda2e921efe384cd9e71f6a220505d24a
Chris Garnett <chris.garnett@open.ac.uk>
2015-10-01 15:49:38 +0100

    Removed references to Autofac just to get test to pass!

--- Autofac/Tests/AutofacTests.cs
+++ Autofac/Tests/AutofacTests.cs
@@ -1,25 +1,36 @@
-﻿using NUnit.Framework;
-
-namespace Autofac.Tests
+﻿namespace Autofac.Tests
 {
+    using Autofac;
+
+    using NUnit.Framework;
+
     [TestFixture]
     public class AutofacTests
     {
         [Test]
         public void ResolveShouldShowTheCorrectGrandchildName()
         {
-            var builder = new ContainerBuilder();
+            //var builder = new ContainerBuilder();
 
-            builder.RegisterType<Grandfather>().As<IGrandfather>();
-            builder.RegisterType<Father>().As<IFather>();
-            builder.RegisterType<Son>().As<ISon>();
-            builder.RegisterType<Logic>();
+            //builder.RegisterType<Grandfather>().As<IGrandfather>();
+            //builder.RegisterType<Father>().As<IFather>();
+            //builder.RegisterType<Son>().As<ISon>();
+            //builder.RegisterType<Logic>();
 
-            var container = builder.Build();
+            //var container = builder.Build();
 
-            var logic = container.Resolve<Logic>();
+            //var logic = container.Resolve<Logic>();
 
-            Assert.AreEqual("This", logic.Run("S"));
+            var son = new Son();
+            var father = new Father();
+            var grandfather = new Grandfather();
+
+            father.Son = son;
+            grandfather.Son = father;
+
+            var logic = new Logic(grandfather, father, son);
+
+            Assert.AreEqual("I am my father's son and my grandfather's grandson. - I am my father's son and my grandfather's grandson.", logic.Run("S"));
         }
     }
 }
