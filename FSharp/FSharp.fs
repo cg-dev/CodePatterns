9c5b1138cd9e599939832228bc0718a05a8f0681
Chris Garnett <chris.garnett@open.ac.uk>
2015-11-03 17:07:01 +0000

    Add new F# project

---
+++ FSharp/FSharp.fs
@@ -0,0 +1,17 @@
+﻿module FSharp
+
+#light  // Compiler directive that simplfies the syntax of the language
+
+let square x = x * x // Defines a function called square which takes a single parameter and squares it. No need to define types as they are inferred.
+let concat (x : string) y = x + " " + y
+
+let numbers = [1 .. 10] //
+
+let squares = List.map square numbers //
+
+printfn "N^2 = %A"squares //
+
+printfn "%s" (concat "Hello" "World!")
+
+open System
+Console.ReadKey(true)
