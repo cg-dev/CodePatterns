3fdb4ea42f43c63e7eec7a7264a33379bb4bc7be
Chris Garnett <chris.garnett@open.ac.uk>
2016-01-26 17:45:19 +0000

    Add Ode2Food images and CSS.

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -1,23 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MVC.Controllers
 {
+    using MVC.Models;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            var message = string.Format("{0}::{1} {2}", controller, action, id);
+
+            ViewBag.Message = message;
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            var model = new AboutModel();
+            model.Name = "Chris";
+            model.Location = "Milton Keynes, United Kingdom";
 
-            return View();
+            return View(model);
         }
 
         public ActionResult Contact()
