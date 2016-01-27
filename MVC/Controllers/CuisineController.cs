50e55e4df74d07652811aba644d915951e38478b
Chris Garnett <chris.garnett@open.ac.uk>
2016-01-27 15:06:50 +0000

    Action filters

--- MVC/Controllers/CuisineController.cs
+++ MVC/Controllers/CuisineController.cs
@@ -2,13 +2,32 @@
 
 namespace MVC.Controllers
 {
+    using System;
+
+    using MVC.Filters;
+
+    [Log]
     public class CuisineController : Controller
     {
-        //
-        // GET: /Cuisine/
+        //[HttpPost] // Action selector
+        //public ActionResult Search(string name = "French")
+        //{
+        //    var message = Server.HtmlEncode(name);
 
+        //    return Content(message);
+        //}
+
+        //[HttpGet] // Action selector
+        //public ActionResult Search()
+        //{
+        //    return Content("Search HttpPost!");
+        //}
+
+        // [Authorize] // Action filter
         public ActionResult Search(string name = "French")
         {
+            throw new Exception("Something dreadful has happened");
+
             var message = Server.HtmlEncode(name);
 
             return Content(message);
