3c42c8cf65ccdb397192eee57587474e342ee596
Chris Garnett <chris.garnett@open.ac.uk>
2016-01-27 14:35:04 +0000

    MVC action results

--- MVC/App_Start/RouteConfig.cs
+++ MVC/App_Start/RouteConfig.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-
-namespace MVC
+﻿namespace MVC
 {
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
@@ -14,10 +10,14 @@ namespace MVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Cuisine",
+                url: "Cuisine/{name}",
+                defaults: new { controller = "Cuisine", action = "Search", name = UrlParameter.Optional });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
---
+++ MVC/Controllers/CuisineController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+
+        public ActionResult Search(string name = "French")
+        {
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+        }
+    }
+}
