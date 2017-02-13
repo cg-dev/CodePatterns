04105df9bf275e2148b64a0841734c400dee95c4
Chris Garnett <chris.garnett@open.ac.uk>
2017-02-13 17:41:06 +0000

    Attempting to understand callbacks using unit tests.

--- OO.Tests/AccountTests.cs
+++ OO.Tests/AccountTests.cs
@@ -10,20 +10,27 @@
         [Test]
         public void DepositingFundsShouldReturnTheCorrectNewBalance()
         {
-            var sut = new Account(() => Console.WriteLine("CallBack executed"));
+            var sut = new Account(UnFreeze);
             sut.Deposit(10);
             sut.Deposit(1);
 
             Assert.AreEqual(11, sut.Balance);
         }
+
         [Test]
         public void DepositingFundsShouldReturnTheCorrectTypeForTheAccountState()
         {
-            var sut = new Account(() => Console.WriteLine("CallBack executed"));
+            var sut = new Account(() => UnFreeze());
             sut.Deposit(10);
             sut.Deposit(1);
 
             Assert.IsInstanceOf(typeof(Active), sut.State);
+        }
+
+        static void UnFreeze()
+        {
+            Console.WriteLine("Callback executing");
+            Console.Read();
         }
     }
 }
