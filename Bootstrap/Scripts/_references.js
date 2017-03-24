3e4fe0407bdbf359fd6cb5bd85ec9476c3aa4dd0
Chris Garnett <chris.garnett@open.ac.uk>
2017-03-24 10:50:57 +0000

    Complete consolidation.

--- Bootstrap/Scripts/_references.js
+++ Bootstrap/Scripts/_references.js
@@ -1,3 +1,3 @@
-﻿/// <reference path="jquery-2.2.0.js" />
-/// <reference path="jquery-ui-1.8.24.js" />
+﻿/// <reference path="jquery-ui-1.11.4.js" />
+/// <reference path="jquery-2.2.0.js" />
 /// <reference path="modernizr-2.6.2.js" />
