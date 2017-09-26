9215fc1205404645a8858d285c5834b9b11cd486
Chris Garnett <chris.garnett@open.ac.uk>
2017-09-26 18:09:00 +0100

    Limit duration to 4 hours.

--- OU.EV/Validations/RequiredForOnCharge.cs
+++ OU.EV/Validations/RequiredForOnCharge.cs
@@ -13,9 +13,9 @@ namespace OU.EV.Validations
 
             if (timeSpan != null)
             {
-                if (slot.Status == Status.OnCharge && (timeSpan < new TimeSpan(0, 15, 0) || timeSpan > new TimeSpan(8, 00, 0)))
+                if (slot.Status == Status.OnCharge && (timeSpan < new TimeSpan(0, 15, 0) || timeSpan > new TimeSpan(4, 00, 0)))
                 {
-                    return new ValidationResult("Duration must be between 00:15:00 and 08:00:00 for a status of On Charge.");
+                    return new ValidationResult("Duration must be between 00:15:00 and 04:00:00 for a status of On Charge.");
                 }
             }
 
