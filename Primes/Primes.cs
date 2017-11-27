1a92aa1110de53f7dd5a2b80d940fcbc3dd1ab60
Chris Garnett <chris.garnett@open.ac.uk>
2017-11-27 10:12:00 +0000

    Call generate method from console application.

--- Primes/Primes.cs
+++ Primes/Primes.cs
@@ -1,9 +1,33 @@
 ﻿namespace Primes
 {
+    using System;
     using System.Collections.Generic;
 
     public static class Primes
     {
+        public static void Main()
+        {
+            const int Number = 13122004;
+
+            Console.WriteLine($"Prime factors for {Number}");
+            Console.WriteLine($"==========================");
+
+            ShowList(Generate(Number));
+
+            Console.WriteLine($"==========================");
+            Console.WriteLine($"Press any key to continue.");
+
+            Console.ReadKey();
+        }
+
+        private static void ShowList(List<int> primeFactors)
+        {
+            foreach (var primeFactor in primeFactors)
+            {
+                Console.WriteLine(primeFactor);
+            }
+        }
+
         public static List<int> Generate(int number)
         {
             var result = new List<int>();
